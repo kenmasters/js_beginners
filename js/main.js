f1e92d9f73b18b248cca9e08fab35afabcfc75ab
KenS <ken.siosan@carespanasia.com>
2023-07-31 08:58:47 +0800

    added examples on how to handle string manipulations

--- js/main.js
+++ js/main.js
@@ -2,3 +2,11 @@ console.log('main.js loaded');
 console.log(typeof 'main.js loaded');
 console.log(typeof 12);
 console.log(typeof true);
+
+const myVariable = 'Mathematics is fun!';
+console.log(myVariable.length);
+console.log(myVariable.charAt(0));
+console.log(myVariable.indexOf('at')); // looks for first occurence of 'at'
+console.log(myVariable.lastIndexOf('at')); // looks for last occurence of 'at'
+console.log(myVariable.slice(0, 2)); // returns a substring from index 0 to 2
+console.log(myVariable.includes('athe')); // returns true if the string contains the specified value
